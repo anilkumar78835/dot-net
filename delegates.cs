ccfc352b57afe85c669ab2d8b452a7f1a4f6007d
anilkumar78835 <69566621+anilkumar78835@users.noreply.github.com>
2021-03-19 16:18:56 +0530

    Add files via upload

---
+++ delegates.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab3
+{
+        public delegate void EventHandler(string a);
+
+        public class Operation
+        {
+            public event EventHandler xyz;
+
+            public void Action(string a)
+            {
+                if (xyz != null)
+                {
+                    xyz(a);
+                    Console.WriteLine(a);
+                }
+                else
+                {
+                    Console.WriteLine("Not Registered");
+                }
+            }
+        }
+
+        class delegates
+    {
+            public static void CatchEvent(string s)
+            {
+                Console.WriteLine("Method Calling");
+            }
+
+            static void Main(string[] args)
+            {
+                Operation o = new Operation();
+
+                o.Action("Event Calling");
+                o.xyz += new EventHandler(CatchEvent);
+
+                Console.ReadLine();
+            }
+        }
+    }
