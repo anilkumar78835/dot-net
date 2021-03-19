0d82215ec383a2cddb80aaa425c5a00490b5926a
anilkumar78835 <69566621+anilkumar78835@users.noreply.github.com>
2021-03-19 16:40:35 +0530

    Add files via upload

---
+++ streamreader_program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+namespace lab6b
+{
+    class streamreader_program
+    {
+        static void Main(string[] args)
+        {
+
+            FileStream f = new FileStream("d:\\b.txt", FileMode.OpenOrCreate);
+
+            Console.WriteLine("File opened");
+
+            
+            StreamReader sr = new StreamReader(f);
+            Console.WriteLine("Reading data from the file");
+
+            string line = sr.ReadLine();
+
+            Console.WriteLine("The data from the file is : " + line);
+
+           
+            sr.Close();
+            f.Close();
+
+            Console.WriteLine("File Stream closed");
+
+            Console.ReadLine();
+        }
+    }
+}
