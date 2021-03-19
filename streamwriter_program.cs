a7d38245a6b78c03084ddbf85b8f9fe2ca006f5b
anilkumar78835 <69566621+anilkumar78835@users.noreply.github.com>
2021-03-19 16:36:10 +0530

    Add files via upload

---
+++ streamwriter_program.cs
@@ -0,0 +1,36 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+ 
+namespace lab6
+{
+    class streamwrite_program
+    {
+        static void Main(string[] args)
+        {
+
+            FileStream f = new FileStream("d:\\b.txt", FileMode.OpenOrCreate);
+
+            Console.WriteLine("File opened");
+
+            
+            StreamWriter s = new StreamWriter(f);
+
+            Console.WriteLine("Writing data to file");
+
+            s.WriteLine("Writing data into file using stream writer");
+
+            
+            s.Close();
+            f.Close();
+
+            Console.WriteLine("File Stream closed");
+
+            Console.ReadLine();
+        }
+    }
+}
